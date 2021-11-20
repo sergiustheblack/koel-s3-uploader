a767ea1a6491ab3953275a0b20db86637ae03133
s.parfyenov <s.parfyenov@zmeke.com>
2021-11-21 00:23:23 +0300

    add AWS support

---
+++ aws.py
@@ -0,0 +1,20 @@
+import main
+import json, logging
+
+
+async def handler(event, context=None, callback=None):
+    for msg in event["Records"]:
+        if msg.get("eventName") == "ObjectCreated:Put" or msg.get("eventName") == "ObjectCreated:CompleteMultipartUpload":
+            action = "create"
+        elif msg.get("eventName") == "ObjectRemoved:Delete" or msg.get("eventName") == "ObjectRemoved:DeleteMarkerCreated":
+            action = "delete"
+        else:
+            logging.error("Unknown s3 operation")
+            raise RuntimeError
+        song = main.S3Song(
+            msg.get("s3").get("bucket").get("name"),
+            msg.get("s3").get("object").get("key"),
+            "https://s3.amazonaws.com",
+            action
+        )
+        await main.handler(song)
